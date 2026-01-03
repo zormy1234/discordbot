3866879651ae997b29ec2a96ba600c5fd85928c6
Zormy Zormo <zormy1234@users.noreply.github.com>
2026-01-03 10:52:26 +0000

    update

--- src/database/PrivateDbTables.ts
+++ src/database/PrivateDbTables.ts
@@ -132,7 +132,7 @@ export async function initDB() {
       bounty_role_id VARCHAR(32) NOT NULL
     );
 
-    CREATE TABLE trader2_players (
+    CREATE TABLE IF NOT EXISTS trader2_players (
       id INT AUTO_INCREMENT PRIMARY KEY,
       timestamp BIGINT NOT NULL,
       playerCount INT NOT NULL,
